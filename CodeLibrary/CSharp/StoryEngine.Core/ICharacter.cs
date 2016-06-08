a580490a3c61b58da3e3f6d226e613c3595891f7
jbn566 <jbn566@gmail.com>
2016-06-08 00:31:36 -0400

    StoryEngine Core

---
+++ CodeLibrary/CSharp/StoryEngine.Core/ICharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.StoryEngine
+{
+    public interface ICharacter<TID, TName, TAttributeIndex, TAttribute, TKind, TTag, TRng> : IObject<TKind, TRng, TID, TTag>,JB2.Common.IPerson<TID>
+        where TID : IComparable
+        where TName : IComparable
+        where TRng : IComparable
+        where TKind : IComparable
+        where TAttribute : ICharacterAttribute<int>
+    {
+        IDictionary<TAttributeIndex, TAttribute> BaseAttributes { get; set; }
+        IEnumerable<ICharacter<TID, TName, TAttributeIndex, TAttribute, TKind, TTag, TRng>> Children { get; set; }
+    }
+}
---
+++ CodeLibrary/CSharp/StoryEngine.Core/ICharacterAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.StoryEngine
+{
+    public interface ICharacterAttribute : ICharacterAttribute<int,Enum.CharacterAttributeType>
+    {
+
+    }
+    public interface ICharacterAttribute<TType> : ICharacterAttribute<int,TType>
+    {
+
+    }
+    public interface ICharacterAttribute<TNumber,TType>
+    {
+        TNumber GetValue();
+        TNumber GetMultiplayer();
+
+        TType GetAttributeType();
+
+        IEnumerable<ICharacterAttribute<TNumber>> GetChildren();
+
+    }
+}
