f4107b937e53dad08e1e468632c571f1e99c64ac
jbn566 <jbn566@gmail.com>
2016-06-29 19:02:24 -0400

    Add Weather Sprog to StoryEngine

--- CodeLibrary/CSharp/StoryEngine.Core/IWorld.cs
+++ CodeLibrary/CSharp/StoryEngine.Core/IWorld.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using JB2.Sprog;
+
 namespace JB2.Engine.Storybook
 {
     public interface IWorld
     {
+        IWeather WeatherCondition { get; set; }
+
+        double MinutesInDay { get; set; }
 
     }
 }
