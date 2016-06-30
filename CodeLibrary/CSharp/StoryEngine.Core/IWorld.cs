3752377825906bb3a98e27b7955ad33ac2411880
Joshua Allen Bennett <jbn566@gmail.com>
2016-06-30 00:00:46 -0400

    Populate the interface for world

--- CodeLibrary/CSharp/StoryEngine.Core/IWorld.cs
+++ CodeLibrary/CSharp/StoryEngine.Core/IWorld.cs
@@ -8,11 +8,15 @@ using JB2.Sprog;
 
 namespace JB2.Engine.Storybook
 {
-    public interface IWorld
+    public interface IWorld<TID> : JB2.Common.IIDProp<TID>
+        where TID : IComparable
     {
         IWeather WeatherCondition { get; set; }
 
         double MinutesInDay { get; set; }
 
+        double Gravity { get; set; }
+
+        IEnumerable<ISpecies<TID>> Species { get; set; }
     }
 }
