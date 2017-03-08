ef00152e32b1866696145be37737cf765ecc4c06
Joshua Allen Bennett <jbn566@gmail.com>
2017-03-08 00:37:24 -0500

    WordRepo

---
+++ CodeLibrary/CSharp/StoryEngine.Core/IWorldRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JB2.Common;
+
+namespace JB2.Engine.Storybook
+{
+    public interface IWorldRepo<TID,TWorldType>
+        where TID : IComparable
+    {
+
+        IWorld<TID, TWorldType> GetWorldByID(string storyBookID, TID id);
+
+        ServiceResult InsertWorld(string storyBookID, IWorld<TID, TWorldType> world);
+
+        ISpecies<TID> GetSpecies(string storyBookID, TID id);
+
+        ServiceResult InsertSpecies(string storyBookID, ISpecies<TID> species);
+    }
+}
